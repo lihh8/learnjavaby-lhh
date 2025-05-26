d430ca73813d35f1941228612dd89a59a11957f1
“lihh” <lihehe66@163.com>
2025-05-26 17:47:25 +0800

    1.add day001-problem.txt and 学习内容.txt;2.add day002-datatype and 学习内容.txt

---
+++ day001/helloword/src/TestMain.java
@@ -0,0 +1,8 @@
+public class TestMain {
+    public static void main(String args[]){
+        System.out.println("姓名：学习的理");
+        System.out.print("目标：一年后进XX"+"\n");
+        System.out.print("薪资：30K");
+        System.out.println("，主攻：java后端");
+    }
+}
